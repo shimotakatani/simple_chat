3267097deda20e12a8d1c675b0aee054515e1962
nponosov <nikita.ponosow@gmail.com>
2017-11-14 20:45:21 +0500

    Initial commit

---
+++ src/ru/nikitos/chat/Chat.java
@@ -0,0 +1,50 @@
+package ru.nikitos.chat;
+
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.json.JSONObject;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static j2html.TagCreator.*;
+import static spark.Spark.*;
+
+public class Chat {
+
+    // this map is shared between sessions and threads, so it needs to be thread-safe (http://stackoverflow.com/a/2688817)
+    static Map<Session, String> userUsernameMap = new ConcurrentHashMap<>();
+    static int nextUserNumber = 1; //Used for creating the next username
+
+    public static void main(String[] args) {
+        staticFileLocation("/public"); //index.html is served at localhost:4567 (default port)
+        webSocket("/chat", ChatWebSocketHandler.class);
+        init();
+    }
+
+    //Sends a message from one user to all users, along with a list of current usernames
+    public static void broadcastMessage(String sender, String message) {
+        userUsernameMap.keySet().stream().filter(Session::isOpen).forEach(session -> {
+            try {
+                session.getRemote().sendString(String.valueOf(new JSONObject()
+                        .put("userMessage", createHtmlMessageFromSender(sender, message))
+                        .put("userlist", userUsernameMap.values())
+                ));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    //Builds a HTML element with a sender-name, a message, and a timestamp,
+    private static String createHtmlMessageFromSender(String sender, String message) {
+        return article().with(
+                b(sender + " says:"),
+                p(message),
+                span().withClass("timestamp").withText(new SimpleDateFormat("HH:mm:ss").format(new Date()))
+        ).render();
+    }
+}
